5381c918cd15d61ef8d456b7ab0dbfde0c7aaf93
guswnsakvk <guswnsakvk1@naver.com>
2021-11-24 12:36:33 +0900

    xmax timer

--- timer/main.js
+++ timer/main.js
@@ -1,13 +1,20 @@
 const timer = document.querySelector("h3.timer")
 
-function getClock(){
-  const date = new Date()
-  const day = String(date.getDay())
-  const hour = String(date.getHours()).padStart(2, "0")
-  const minute = String(date.getMinutes()).padStart(2, "0")
-  const second = String(date.getSeconds()).padStart(2, "0")
-  timer.innerText = `${day}d ${hour}h ${minute}m ${second}s`
+const second = 1000
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24
+
+function getTime(){
+  const xmax = new Date("2021-12-25:00:00:00+0900")
+  const now = new Date()
+  const gap = xmax - now
+  const gap_day = Math.floor(gap / day)
+  const gap_hour = String(Math.floor(gap % day / hour)).padStart(2,"0")
+  const gap_minute = String(Math.floor(gap % hour / minute)).padStart(2, "0")
+  const gap_second = String(Math.floor(gap % minute / second)).padStart(2,"0")
+  timer.innerText = `${gap_day}d ${gap_hour}h ${gap_minute}m ${gap_second}s `
 }
 
-getClock()
-setInterval(getClock, 1000)
+getTime()
+setInterval(getTime, 1000)
